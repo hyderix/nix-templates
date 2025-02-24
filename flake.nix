aa9e8b7d2b9d176b300cc31efe0f1435a16f0f6e
Eric Thorburn <hyderix@protonmail.com>
2025-02-24 15:04:54 +0100

    init flake

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "hyderix collection of nix templates";
+
+  outputs = { self }: {
+    templates = {
+      unstable-flake = {
+        path = ./unstable-flake;
+        description = "A basic flake template with unstable nixpkgs";
+      };
+    };
+  };
+}
