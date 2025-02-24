21458db9ecf4f6a9b11aa02af8bae736ff8ab588
Eric Thorburn <hyderix@protonmail.com>
2025-02-24 15:27:57 +0100

    add nixpkgs to root thingy

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   description = "hyderix collection of nix templates";
 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
   outputs = {self}: {
     templates = {
       unstable-flake = {
